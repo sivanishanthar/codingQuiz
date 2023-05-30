a69d28f8f5989bd658f7e3bf281db2d89e2c16e5
Siva <r.nishanthar@gmail.com>
2023-05-29 22:39:35 -0400

    initial commit

---
+++ assets/js/questions.js
@@ -0,0 +1,8 @@
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: 'Commonly used data types DO NOT include:',
+    choices: ['strings', 'booleans', 'alerts', 'numbers'],
+    answer: 'alerts',
+  },
+]
