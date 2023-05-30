2622165ada3b0e56823394bf4c4fad26f26db945
Siva <r.nishanthar@gmail.com>
2023-05-29 23:00:48 -0400

    question logic

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -6,9 +6,15 @@ var questions = [
     answer: '<script>',
   },
   {
-    title: 'The condition in an if / else statement is enclosed within ____.',
-    choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
-    answer: 'parentheses',
+    title: 'Javascript is an ______ language',
+    choices: ['object-oriented', 'object-base', 'procedural', 'none of the above'],
+    answer: 'object-oriented',
+  },
+  {
+    title:
+      'A very useful tool used during development and debugging for printing content to the debugger is:',
+    choices: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
+    answer: 'console.log',
   },
   {
     title: 'Arrays in JavaScript can be used to store ____.',
