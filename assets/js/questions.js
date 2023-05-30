090596da361f3ba220b98a1bd66ffa125048784e
Siva <r.nishanthar@gmail.com>
2023-05-29 22:50:51 -0400

    questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,8 +1,29 @@
 // list of all questions, choices, and answers
 var questions = [
   {
-    title: 'Commonly used data types DO NOT include:',
-    choices: ['strings', 'booleans', 'alerts', 'numbers'],
-    answer: 'alerts',
+    title: 'Inside with HTML element do we put the javascript',
+    choices: ['<script>', '<js>', '<javascript>', 'scripting'],
+    answer: '<script>',
+  },
+  {
+    title: 'The condition in an if / else statement is enclosed within ____.',
+    choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+    answer: 'parentheses',
+  },
+  {
+    title: 'Arrays in JavaScript can be used to store ____.',
+    choices: [
+      'numbers and strings',
+      'other arrays',
+      'booleans',
+      'all of the above',
+    ],
+    answer: 'all of the above',
+  },
+  {
+    title:
+      'String values must be enclosed within ____ when being assigned to variables.',
+    choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+    answer: 'quotes',
   },
 ]
